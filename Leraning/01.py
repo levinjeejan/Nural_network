2e4fd3681f415c96858684d5d8c5ee6538b55b59
levinjeejan <levinjeejan@gmail.com>
2022-08-23 20:48:05 +0530

    new file

---
+++ Leraning/01.py
@@ -0,0 +1,7 @@
+from xml.sax.xmlreader import InputSource
+
+
+Inputs = [1,2,3,4,5]
+weights = [6,7,8,9,10]
+bias = 3
+output = Inputs[0]*weights[0]+  Inputs[1]*weights[1]+  Inputs[2]*weights[2]+ bias
