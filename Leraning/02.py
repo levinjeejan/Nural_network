21d9c62815716ab73897d81647d02b5e8019ca58
levinjeejan <levinjeejan@gmail.com>
2022-08-23 22:04:12 +0530

    23/8/2022

--- Leraning/02.py
+++ Leraning/02.py
@@ -1,5 +1,14 @@
-Inputs = [1,2,3,4,5]
-weights = [6,7,8,9,10]
-bias = 3
-output = Inputs[0]*weights[0]+  Inputs[1]*weights[1]+  Inputs[2]*weights[2]+ bias
+Inputs = [1,2,3,2.5]
+
+weights1 = [0.2,0.8,-0.5,1.0]
+weights2 = [0.5,-0.9,0.26,-0.5]
+weights3 = [-0.26,-0.27,0.17,0.87]
+
+bias1 = 2
+bias2 = 3
+bias3 = 0.5
+
+output = [Inputs[0]*weights1[0]+  Inputs[1]*weights1[1]+  Inputs[2]*weights1[2]+ Inputs[3]*weights1[3]+ bias1,
+          Inputs[0]*weights2[0]+  Inputs[1]*weights2[1]+  Inputs[2]*weights2[2]+ Inputs[3]*weights2[3]+ bias2,
+          Inputs[0]*weights3[0]+  Inputs[1]*weights3[1]+  Inputs[2]*weights3[2]+ Inputs[3]*weights3[3]+ bias3]
 print(output)
